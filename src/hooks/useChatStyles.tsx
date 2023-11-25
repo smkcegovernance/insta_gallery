c1067ab09138e66f80ce2119ffbb79e6ae1bfc0a
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-11-25 12:24:11 +0530

    useChatStyles hook created

---
+++ src/hooks/useChatStyles.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import useScaffoldStyles from './useScaffoldStyles';
+import {StyleSheet} from 'react-native';
+
+export default function useChatStyles() {
+  const styles = useScaffoldStyles();
+  return React.useMemo(
+    () =>
+      StyleSheet.create({
+        ...styles,
+        statusbar: {
+          backgroundColor: '#212121',
+        },
+        scaffold: {
+          ...styles.scaffold,
+          backgroundColor: '#121212',
+        },
+        appbar: {
+          ...styles.appbar,
+          backgroundColor: '#212121',
+        },
+        myMessage: {
+          backgroundColor: '#212121',
+          padding: 16,
+          borderRadius: 16,
+          borderBottomRightRadius: 0,
+          margin: 8,
+          maxWidth: '90%',
+          alignSelf: 'flex-end',
+        },
+        myMessageText: {
+          fontSize: 18,
+        },
+        otherMessage: {
+          backgroundColor: '#212121',
+          padding: 16,
+          borderRadius: 16,
+          borderTopLeftRadius: 0,
+          margin: 8,
+          maxWidth: '90%',
+          alignSelf: 'flex-start',
+        },
+        otherMessageText: {
+          fontSize: 18,
+        },
+        newMessageInputBar: {
+          backgroundColor: '#212121',
+        },
+        newMessageInputContent: {
+          fontWeight: '600',
+          fontSize: 18,
+        },
+        newMessageInputBarOutline: {
+          borderRadius: 0,
+          borderWidth: 0,
+        },
+      }),
+    [styles],
+  );
+}
