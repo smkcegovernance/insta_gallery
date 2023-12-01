71bb00c337368f1ae74c2f8ac43cc30adad3734c
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-12-01 22:27:37 +0530

    IMessage type is simplified

--- src/models/IMessage.ts
+++ src/models/IMessage.ts
@@ -2,16 +2,18 @@ import {IPost, IPostResult} from './IPost';
 
 type IMessageType = 'text' | 'image';
 type IMessageDirection = 'in' | 'out';
-type IImage = {
+export type IImage = {
   width: number;
   height: number;
   url: string;
 };
 export type IMessage = {
-  text?: string;
+  type: IMessageType;
   direction: IMessageDirection;
-  type?: IMessageType;
-  image?: IImage;
+  text?: string;
+  imageWidth?: number;
+  imageHeight?: number;
+  imageUrl?: string;
 };
 
 export type IMessages = IMessage[];
@@ -28,7 +30,9 @@ export const newIncomingTextMessage = (text: string): IMessage => ({
   type: 'text',
 });
 export const newIncomingImageMessage = (image: IImage): IMessage => ({
-  image: image,
+  imageHeight: image.height,
+  imageWidth: image.width,
+  imageUrl: image.url,
   direction: 'in',
   type: 'image',
 });
@@ -36,13 +40,9 @@ export const newIncomingImageMessage = (image: IImage): IMessage => ({
 export const messagesFromPost = (value: IPost): IMessages => {
   let _messages: IMessages = [newIncomingTextMessage(value.UserName)];
   if (value.ProductType === 'clips') {
-    _messages = _messages.concat(
-      newIncomingTextMessage('Clips are not supported yet'),
-    );
+    _messages.push(newIncomingTextMessage('Clips are not supported yet'));
   }
-  _messages = _messages.concat(
-    value.Media.map(media => newIncomingImageMessage(media)),
-  );
+  value.Media.forEach(media => _messages.push(newIncomingImageMessage(media)));
   return _messages;
 };
 
